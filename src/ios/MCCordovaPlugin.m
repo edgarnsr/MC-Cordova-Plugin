b3cbe6ad71ad268ebef8fdbed7c96509270db8e5
Edgar Noel Salmerón Ramírez <12400976+edgarnsr@users.noreply.github.com>
2023-02-27 17:48:24 -0700

    Update MCCordovaPlugin.m to prevent auto request permission

--- src/ios/MCCordovaPlugin.m
+++ src/ios/MCCordovaPlugin.m
@@ -152,7 +152,6 @@ const int LOG_LENGTH = 800;
             [self setDelegate];
             [[MarketingCloudSDK sharedInstance] sfmc_setURLHandlingDelegate:self];
             [[MarketingCloudSDK sharedInstance] sfmc_addTag:@"Cordova"];
-            [self requestPushPermission];
         } else if (configError != nil) {
             os_log_debug(OS_LOG_DEFAULT, "%@", configError);
             if (configError.code == configureInvalidAppEndpointError) {
